13981f1e8d9a3021e7a998454f00a3bada50e51e
Shriram26101999 <108632713+Shriram26101999@users.noreply.github.com>
2023-03-10 19:42:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,79 +1,83 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-    constructor(props){
-        super(props)
-        this.calculate=this.calculate.bind(this)
-        this.reset=this.reset.bind(this)
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [relationship, setRelationship] = useState("");
+  
+  const handleClick = () => {
+    if (firstName === "" || secondName === "") {
+      setRelationship("Please Enter valid input");
+      return;
     }
-    state={
-        firstName:"",
-        lastName:"",
-        result:""
+    
+    const name1 = firstName.toLowerCase();
+    const name2 = secondName.toLowerCase();
+    
+    let commonChars = "";
+    for (let i = 0; i < name1.length; i++) {
+      const char = name1.charAt(i);
+      if (name2.includes(char)) {
+        commonChars += char;
+        name2 = name2.replace(char, "");
+      }
     }
-
-    calculate(e)
-    {
-        e.preventDefault();
-        let a =this.state.firstName
-        let b =this.state.lastName
-
-        let resultA = a.split('').filter((elem) => b.indexOf(elem) == -1).join('');
-        let resultB = b.split('').filter((elem) => a.indexOf(elem) == -1).join('');
-
-        console.log(resultB);
-        console.log(resultA);
-
-        var result=(resultA.length+resultB.length)%6
-        if(this.state.firstName=="" && this.state.secondName=="")
-        {
-            this.setState({result:"Please Enter valid input"})
-        }
-        else if(result==1)
-        {
-            this.setState({result:"Friends"})
-        }
-        else if(result==2)
-        {
-            this.setState({result:"Love"})
-        }
-        else if(result==3)
-        {
-            this.setState({result:"Affection"})
-        }
-        else if(result==4)
-        {
-            this.setState({result:"Marriage"})
-        }
-        else if(result==5)
-        {
-            this.setState({result:"Enemy"})
-        }
-        else if(result==0)
-        {
-            this.setState({result:"Siblings"})
-        }
+    
+    const remaining1 = name1.replace(new RegExp(`[${commonChars}]`, "g"), "");
+    const remaining2 = name2.replace(new RegExp(`[${commonChars}]`, "g"), "");
+    const sum = (remaining1.length + remaining2.length) % 6;
+    
+    switch (sum) {
+      case 0:
+        setRelationship("Siblings");
+        break;
+      case 1:
+        setRelationship("Friends");
+        break;
+      case 2:
+        setRelationship("Love");
+        break;
+      case 3:
+        setRelationship("Affection");
+        break;
+      case 4:
+        setRelationship("Marriage");
+        break;
+      case 5:
+        setRelationship("Enemy");
+        break;
+      default:
+        setRelationship("Please Enter valid input");
     }
+  };
+  
+  const handleClear = () => {
+    setFirstName("");
+    setSecondName("");
+    setRelationship("");
+  };
 
-    reset(){
-        this.setState({result:""})
-    }
-    render() {
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-                <h3 data-testid="answer">{this.state.result}</h3><br/>
-                <form>
-            <label>First Name: <input data-testid="input1" onChange={(e)=>this.setState({firstName:e.target.value})}/></label><br/><br/>
-                <label>Second Name: <input data-testid="input2" onChange={(e)=>this.setState({lastName:e.target.value})}/></label><br/><br/>
-                <button data-testid="calculate_relationship" onClick={(e)=>this.calculate(e)}>Calculate Relationship Future</button>
-                <button data-testid="clear" onClick={()=>this.reset()} type="reset" defaultValue="Reset">Clear Button</button>
-                </form>
-            </div>
-        )
-    }
+  return (
+    <div>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        data-testid="input1"
+      />
+      <input
+        type="text"
+        value={secondName}
+        onChange={(e) => setSecondName(e.target.value)}
+        data-testid="input2"
+      />
+      <button onClick={handleClick} data-testid="calculate_relationship">
+        Calculate Relationship Future
+      </button>
+      <button onClick={handleClear}>Clear</button>
+      <h3 data-testid="answer">{relationship}</h3>
+    </div>
+  );
 }
 
-
-export default App
+export default App;
